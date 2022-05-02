31f30dc5e47a80bf9fbdf16da9fbdd90b2469f21
M4ss1ck <4ndyraul@gmail.com>
2022-05-02 03:04:56 -0400

    bigger buttons

--- src/index.js
+++ src/index.js
@@ -1242,7 +1242,26 @@ function handleBoard() {
 
   const startBtn = document.createElement("button");
   startBtn.classList.add("startBtn");
-  startBtn.textContent = "Play Again";
+  // Add svg icon
+  //   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  //   <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+  // </svg>
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("class", "h-6 w-6");
+  svg.setAttribute("fill", "none");
+  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("stroke", "currentColor");
+  svg.setAttribute("stroke-width", "2");
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("stroke-linecap", "round");
+  path.setAttribute("stroke-linejoin", "round");
+  path.setAttribute(
+    "d",
+    "M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+  );
+  svg.appendChild(path);
+  startBtn.appendChild(svg);
+  //startBtn.textContent = "Play Again";
   startBtn.onpointerdown = () => {
     startAgain();
   };
